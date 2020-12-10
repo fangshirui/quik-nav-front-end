28f422e7194e293f87131299d992ff1612cf7a16
kk <fsrgyx@gmail.com>
2020-12-10 15:39:35 +0800

    sourceMap说明

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,9 @@ module.exports = {
       }
     }
   },
-  // 生产环境 是否需要 source map, 默认为true
-  // 定义为false可以优化前端代码量
+  // 生产环境 是否需要 sourceMap, 默认为true
+  // sourceMap对普通用户无感，
+  // 浏览器打开开发者模式才会加载后端的map文件帮助分析
+  // 若想在前端隐藏隐藏源代码可以考虑关闭sourceMap
   productionSourceMap: false
 }
